a3c344e7ea4adea1e273a0f6bc2d1f4c417d5bd5
Gavin O'Connor <gedmund@gmail.com>
2019-03-28 18:04:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // takeANumber adds customer to line
 var takeANumber = function(katzDeliLine, newCustomer) {
   for (i = 0; i < katzDeliLine.length; i++) {
-    katzDeliLine.push(`Welcome, ${newCustomer}. You are number ${katzDeliLine.length + 1} in line.`)
+    katzDeliLine.push(`Welcome, ${newCustomer[i]}. You are number ${katzDeliLine.length + 1} in line.`)
   }  
 }
 // nowServing announces customer, removes them from array, returns message if empty
