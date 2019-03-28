27f14059a3783f977e5a8a92e45c2ffe2d26dd87
Gavin O'Connor <gedmund@gmail.com>
2019-03-28 18:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ var currentLine = function(katzDeliLine) {
 
   for (var i = 0; i < katzDeliLine.length; i++) {
     thisLine.push(` ${i + 1}. ${katzDeliLine[i]}`)
-  } if (katzDeliLine.length === 0) {
+  }
+    if (katzDeliLine.length === 0) {
     return `The line is currently empty.`
   } else {
     return `The line is currently:${thisLine }`
