12972fce7fbe8f0f21a2e6fb97905eafd35f1ef4
Gavin O'Connor <gedmund@gmail.com>
2019-03-28 18:11:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var takeANumber = function(katzDeliLine, newCustomer) {
     return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`
 }
 // nowServing announces customer, removes them from array, returns message if empty
+var nowServing = function(katzDeliLine) {
+  var i = 0
 
+  while (i < katzDeliLine.length) {
+    i++
+  }
+  if (katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served!`
+  } else {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+}
 
 // currentLine accepts current line of people and returns string
